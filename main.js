c3b268cc77fceca9c4dd1f559bb4d46bd17aa1dc
felix tineo <ftandcompany@gmail.com>
2020-10-21 12:13:06 -0400

    read excel

--- main.js
+++ main.js
@@ -1,21 +1,47 @@
 const puppeteer = require('puppeteer');
+const fs = require("fs");
+const xlsx = require('node-xlsx').default;
 
 const site = "https://nexxos.prop360.cl/index.aspx";
 const input = {
-  mail: "equipo@nexxos.cl",
-  pass: "Nexxos.2020",
+  mail: "contacto@nexxos.cl",
+  pass: "Nexxos2020",
 };
 
 (async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto(site);
-  await page.type("#tbMail", input.mail, { delay: 100 });
-  await page.type("#tbPassword", input.pass, { delay: 100 });
-  await page.click("#botLogin");
-  await page.waitForNavigation();
+  try{
+    const browser = await puppeteer.launch();
 
-  await page.screenshot({path: 'screenshot.png'});
+    const page = await browser.newPage();
+    await page.goto(site);
+    console.log("Login - Home");
+    
+    await page.type("#tbMail", input.mail, { delay: 100 });
+    await page.type("#tbPassword", input.pass, { delay: 100 });
+    await page.click("#botLogin");
+    console.log("Usuario y contraseña enviados, Esperando respuesta...");
+    
+    await page.waitForNavigation({ waitUntil: "networkidle0" });
+    console.log("Dashboard");
+    
+    await page.goto("https://nexxos.prop360.cl/backOffice/propiedades/propiedades.aspx");
+    console.log("Propiedades");
+    
+    await page._client.send('Page.setDownloadBehavior', { behavior: 'allow', downloadPath: './downloads', name: "some.xml" });
+    await page.click("#listadoExcel");
+    console.log("Descargando...");
+    await page.waitFor(60000);
 
-  await browser.close();
+    console.log("Guardado en /RUTA_DEL_PROYECTO/dowloads");  
+    await browser.close();
+
+    const files = await fs.readdirSync('./downloads');
+    console.log("FILES", files);
+
+    const result = xlsx.parse(fs.readFileSync(`downloads/${files[0]}`));
+    console.log("RESULT", result[0].data[1]);
+  }catch(e){
+    console.log("ERROR",e);
+    await browser.close();
+  }
 })();
